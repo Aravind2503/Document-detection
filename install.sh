d9a4cd79a7e3ab899bc1070d76ebf22ade26d02c
Aravind S <s.aravind2000@gmail.com>
2021-02-28 11:07:28 +0530

    added install.sh and readme changes

---
+++ install.sh
@@ -0,0 +1,46 @@
+#installing python venv
+echo installing python venv...
+sudo apt install python3-venv;
+
+#creating the virtual environment
+echo creating virtual env...
+cd python; #inside python directory
+python3 -m venv doc-detect-env;
+
+#activating virtual environment
+source doc-detect-env/bin/activate;
+
+#installing python dependencies
+echo "installing python libraries..."
+pip3 install -r requirements.txt;
+
+#installing tesseract ocr
+echo "installing tesseract-ocr..."
+sudo apt-install tesseract-ocr;
+
+#installing python-3tk
+echo "installing python-3tk for GUI"
+sudo apt-install python3-tk;
+
+#deactivating virtual environment
+deactivate;
+
+#coming back to home directory
+cd ..;
+
+#making required directories
+echo 'making the required directories...'
+mkdir python/resources/preprocessing_output;
+mkdir python/resources/ml;
+mkdir python/resources/ml/buffer;
+mkdir python/resources/ml/test;
+mkdir python/resources/ml/train;
+mkdir python/resources/ml/buffer/test;
+mkdir python/resources/ml/buffer/train;
+
+
+#installing all the node dependencies
+echo 'installing all node dependencies...'
+npm install;
+
+echo 'installation complete :)'
--- readme.md
+++ readme.md
@@ -1,28 +1,4 @@
-creating python virtual environment
- - open the python folder
- - use the commands
-    - "sudo apt install python3-venv"
-
-    - "python3 -m venv doc-detect-env" doc detect env is the name of the virtual environment 
-
-    - To start using this virtual environment, you need to activate it by running the activate script:
-
-        "source my-project-env/bin/activate"
-    
-    - inside the virtual environment type "pip/pip3 install -r requirements.txt"
-
-    - type "deactivate" to leave the virtual environment
-    
-    - also need to add additional resources folder
-
-    - run "sudo apt-install tesseract-ocr"
-    
-    - run "sudo apt-install python3-tk"
-    
-
-
-
-- run "npm install" to install all the node dependencies
+- run install.sh
 
 - use "npm run start" to run using node
 
